da8bf92af41d138e78c15a48f749e848bc49c18b
Federico Luna <fluna1973@yahoo.com>
2022-10-25 15:38:22 -0600

    Update schema_based_on_diagram.sql
    Add Keys and index

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -52,3 +52,52 @@ ALTER TABLE medical_histories
 
 
 COMMIT;
+
+BEGIN;
+
+ALTER TABLE invoices
+  ADD CONSTRAINT fk_invoices
+  FOREIGN KEY (medical_history_id)
+  REFERENCES medical_histories (id);
+  
+ALTER TABLE invoice_items
+  ADD CONSTRAINT fk_invoice_id
+  FOREIGN KEY (invoice_id)
+  REFERENCES invoices (id);
+  
+ALTER TABLE invoice_items
+  ADD CONSTRAINT fk_treatments
+  FOREIGN KEY (treatment_id)
+  REFERENCES treatments (id);
+  
+SAVEPOINT  createOnToManyRel;
+
+CREATE TABLE treatments_medical_histories (
+  treatment_id INT NOT NULL,
+  medical_history_id INT NOT NULL,
+  CONSTRAINT treatments_medical_historiess_pkey PRIMARY KEY (treatment_id, medical_history_id)
+);
+
+ALTER TABLE treatments_medical_histories
+  ADD CONSTRAINT fk_treatment_id
+  FOREIGN KEY (treatment_id)
+  REFERENCES treatments (id);
+  
+ALTER TABLE treatments_medical_histories
+  ADD CONSTRAINT fk_medical_history_id
+  FOREIGN KEY (medical_history_id)
+  REFERENCES medical_histories (id);
+  
+SAVEPOINT  createMAnyToManyRel;
+
+CREATE INDEX med_pati ON medical_histories (patient_id);
+
+CREATE INDEX med_hist ON invoices (medical_history_id);
+
+CREATE INDEX inv_item_invoices ON invoice_items (invoice_id);
+
+CREATE INDEX inv_item_treatment ON invoice_items (treatment_id);
+
+SAVEPOINT  createSimpleIndexs;
+
+COMMIT;
